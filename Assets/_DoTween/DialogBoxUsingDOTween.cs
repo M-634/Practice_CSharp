d77282ff255df3e8e72ac5b2c103ae209eb50e38
teruteru634 <miyanagateru634@gmail.com>
2020-08-25 19:02:15 +0900

    DoTweenを使った簡易的なダイアログUI

---
+++ Assets/_DoTween/DialogBoxUsingDOTween.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
+
+/// <summary>
+/// ダイアログUIのコントロールを制御するクラス
+/// </summary>
+public class DialogBoxUsingDOTween : MonoBehaviour
+{
+    [SerializeField] Text m_text;
+    [SerializeField] Button m_openButton;
+    [SerializeField] Button m_closeButton;
+
+    [SerializeField] Vector3 m_openScale = Vector3.one;
+    [SerializeField] float m_openSpeeDuration = 1f;
+    [SerializeField] float m_closeSpeeDuration = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.transform.localScale = Vector3.zero;
+        RestDialog();
+    }
+
+    private void RestDialog()
+    {
+        gameObject.SetActive(false);
+        m_openButton.interactable = true;
+        m_closeButton.interactable = false;
+    }
+
+    public void OpenBottunClick()
+    {
+        gameObject.SetActive(true);
+        m_openButton.interactable = false;
+
+        DOTween.Sequence()
+            .Append(this.transform.DOScale(m_openScale, m_openSpeeDuration))
+            .Append(DOVirtual.Float(0f, 100f, 3f, value => { m_text.text = "Count; " + (int)value; })
+            .OnComplete(() => m_closeButton.interactable = true));
+    }
+
+    public void CloseButtunClick()
+    {
+        DOTween.Sequence()
+           .Append(this.transform.DOScale(Vector3.zero, m_closeSpeeDuration))
+           .OnComplete(() => RestDialog());
+    }
+}

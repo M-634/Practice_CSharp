bc2756db7681a113e65bf2957f762c3e05507c55
teruteru634 <miyanagateru634@gmail.com>
2020-10-10 15:21:07 +0900

    Unity Eventを使ったEvent処理の例。こっちのほうが汎用性が高いかも？

---
+++ Assets/_Event/InputListener.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UIElements;
+using UnityEngine.XR.WSA.Input;
+
+namespace Practice_Event
+{
+    public class InputListener : MonoBehaviour
+    {
+        [SerializeField] UnityEvent m_onButtonDown;
+        [SerializeField] UnityEvent m_onButtonUp;
+        [SerializeField] Input Input;
+
+        private void Start()
+        {
+            m_onButtonDown.AddListener(() => Debug.Log("press"));
+        }
+
+        private void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                m_onButtonDown.Invoke();
+            }
+            else if(Input.GetMouseButtonUp(0))
+            {
+                m_onButtonUp.Invoke();
+            }
+        }
+
+    }
+}
+
